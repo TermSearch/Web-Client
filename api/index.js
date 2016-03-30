8ea96fccb7a06d9ee310dfe0e212cb24a339afa4
Koen van Gilst <koen.vangilst@gmail.com>
2016-03-30 16:06:23 +0200

    incorporates API changes

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@
 //
 export default function search({ term, selectedSubjectFields }) {
   const apiUrl = 'http://localhost:2020/api/dictentries/startsWith?';
-  const queryString = `term=${term}&limit=100&skip=0&subjectFieldStr=${selectedSubjectFields}`;
+  const queryString = `term=${term}&limit=100&skip=0&subjectFields=${selectedSubjectFields}`;
   const url = apiUrl + queryString;
   // console.log(`Query: ${url}`);
   return fetch(url, { method: 'get' })
