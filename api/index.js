2b38cc23fdff7ce406f612c4cb0f983949d7a79a
Koen van Gilst <koen.vangilst@gmail.com>
2016-03-31 16:10:27 +0200

    adds testing mocha + babel

--- api/index.js
+++ api/index.js
@@ -7,6 +7,6 @@ export default function search({ term, selectedSubjectFields }) {
   const url = apiUrl + queryString;
   // console.log(`Query: ${url}`);
   return fetch(url, { method: 'get' })
-  .then(res => res.json())
-  .then(json => json.results.dictentries || []); // should return an empty array if not found
+    .then(res => res.json())
+    .then(json => json.results.dictentries || []); // should return an empty array if not found
 }
---
+++ tests/api-test.js
@@ -0,0 +1,18 @@
+//
+// Copyright (c) 2016 by Koen van Gilst (@vnglst) | MIT license
+//
+
+import { describe, it } from 'mocha';
+import { expect } from 'chai';
+// import api from '../api/index.js';
+
+//
+// generatePriceOptions
+//
+
+describe('API should return', () => {
+  const priceOptions = 0;
+  it('Should be 0', () => {
+    expect(priceOptions).to.be.a('number');
+  });
+});
