fc02da91b0b62e8bb5c57ea5c23c3846844cd131
Koen van Gilst <koen.vangilst@gmail.com>
2016-05-06 22:25:30 +0200

    removes logger from production

--- store/configureStore.js
+++ store/configureStore.js
@@ -1,12 +1,23 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import createLogger from 'redux-logger';
 import reducer from '../reducers';
 
-const createStoreWithMiddleware = applyMiddleware(
-  thunk,
-  createLogger() /* TODO: Disable logging in production */
-)(createStore);
+// const createStoreWithMiddleware = applyMiddleware(
+//   thunk,
+//   createLogger() /* TODO: Disable logging in production */
+// )(createStore);
+
+let middleware = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+  // using require here for conditional, import not allowed
+  const createLogger = require('redux-logger');
+  middleware = [...middleware, createLogger()];
+} else {
+  // middleware in production
+}
+
+const createStoreWithMiddleware = applyMiddleware(...middleware)(createStore);
 
 export default function configureStore(initialState) {
   const store = createStoreWithMiddleware(reducer, initialState);
