53382cd2a89e29e16a535c22fa214ff1372149bf
Koen van Gilst <koen.vangilst@gmail.com>
2016-06-07 17:12:41 +0200

    updates tests for nl dups

--- tests/mergeDuplicats-test.js
+++ tests/mergeDuplicats-test.js
@@ -5,49 +5,34 @@ test('Merging duplicates', (assert) => {
 
   const input = [
     {
-      "id": "55a7bdd97af068e81b7f74c8",
       "de": "Anlage",
-      "deUrl": "Anlage",
       "nl": [
           "bijl.",
-          "bijlage"
+          "bijlage",
         ],
-      "note": "TRAD - Everyday terms and expressions",
-      "subjectFields": []
       },
     {
-      "id": "55a7bdda7af068e81b7f88f2",
       "de": "Anlage",
-      "deUrl": "Anlage",
       "nl": [
           "bijl.",
-          "bijlage"
+          "bijlage",
         ],
-      "note": "TRAD - Everyday terms and expressions",
-      "subjectFields": []
       }
     ]
 
-
   const expected = [
     {
-      "id": "55a7bdd97af068e81b7f74c8",
       "de": "Anlage",
-      "deUrl": "Anlage",
       "nl": [
         "bijl.",
         "bijlage",
-        "bijl.",
-        "bijlage"
-    ],
-      "note": "TRAD - Everyday terms and expressions",
-      "subjectFields": []
+      ],
     }
   ];
   const actual = mergeDuplicates(input);
 
   assert.deepEqual(actual, expected,
-    'Dutch translations should be merged.');
+    'All German & Dutch duplicates should be merged.');
 
   assert.end();
 });
