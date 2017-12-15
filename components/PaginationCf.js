e36ed45bac5516a32b2ec0eca3e6947e7234d949
Koen van Gilst <koen.vangilst@gmail.com>
2017-12-15 14:49:18 +0100

    fix for firefox

---
+++ components/PaginationCf.js
@@ -0,0 +1,76 @@
+const React = require('react');
+const {render} = require('react-dom');
+const {PaginationBuilder} = require('cf-builder-pagination');
+
+function getStartAndEnd(page, pageSize) {
+  const start = (page - 1) * pageSize;
+  const end = start + pageSize - 1;
+  return [start, end];
+}
+
+function hasAllItems(items, start, end) {
+  for (let i = start; i <= end; i++) {
+    if (!items[i] || items[i].isRequesting) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+class Component extends React.Component {
+  state = {
+    items: [],
+    totalCount: 143,
+    page: 1,
+    perPage: 20
+  };
+
+  componentDidMount() {
+    this.maybeRequestPage(this.state.page);
+  }
+
+  handlePageChange = page => {
+    this.maybeRequestPage(page);
+  };
+
+  // This is mimicking what will happen in the API actions/reducers:
+  maybeRequestPage(page) {
+    const items = this.state.items.slice();
+    const [start,
+      end] = getStartAndEnd(page, this.state.perPage);
+    const needsRequest = !hasAllItems(items, start, end);
+
+    if (!needsRequest) {
+      this.setState({page});
+      return;
+    }
+
+    for (let i = start; i <= end; i++) {
+      if (!items[i]) {
+        items[i] = {
+          isRequesting: true
+        };
+      }
+    }
+
+    this.setState({page, items});
+
+    setTimeout(() => {
+      const items = this.state.items.slice();
+
+      for (let i = start; i <= end; i++) {
+        items[i].isRequesting = false;
+      }
+
+      this.setState({items});
+    }, 500);
+  }
+
+  render() {
+    const [start, end] = getStartAndEnd(this.state.page, this.state.perPage);
+
+
+    return (<PaginationBuilder onPageChange={this.handlePageChange} totalCount={this.state.totalCount} page={this.state.page} perPage={this.state.perPage}/>);
+  }
+}
